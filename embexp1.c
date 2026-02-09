642b6df4f99202d0cdd3bbb401bc6636651272ca
Abhinav <83628416+abhinavuser@users.noreply.github.com>
2026-02-09 14:33:16 +0530

    Add files via upload

---
+++ embexp1.c
@@ -0,0 +1,15 @@
+#include<reg51.h>
+void delay(unsigned int);
+sbit byte = P1^1;
+void main(void){
+while(1)
+{
+byte=-byte;
+delay(306);
+}
+}
+void delay(unsigned int n)
+{
+int j;
+for(j=0;j<=n;j++);
+}
